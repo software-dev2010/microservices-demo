0fd401ec07bee5808bb1f5df59abba7fbfd7ffeb
D.S <daianaciuciulete@gmail.com>
2023-04-09 16:50:45 +0300

    added elasticsearch

---
+++ elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticRepositoryIndexClient.java
@@ -0,0 +1,36 @@
+package com.microservices.demo.elastic.index.client.service.impl;
+
+import com.microservices.demo.elastic.index.client.repository.TwitterElasticsearchIndexRepository;
+import com.microservices.demo.elastic.index.client.service.ElasticIndexClient;
+import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "elastic-config.is-repository", havingValue = "true", matchIfMissing = true)
+public class TwitterElasticRepositoryIndexClient implements ElasticIndexClient<TwitterIndexModel> {
+
+    private final TwitterElasticsearchIndexRepository twitterElasticsearchIndexRepository;
+
+    @Override
+    public List<String> save(List<TwitterIndexModel> documents) {
+        List<TwitterIndexModel> repositoryResponse =
+                (List<TwitterIndexModel>) twitterElasticsearchIndexRepository.saveAll(documents);
+
+        List<String> ids = repositoryResponse.stream()
+                .map(TwitterIndexModel::getId)
+                .collect(Collectors.toList());
+
+        log.info("Documents indexed successfully with type: {} and ids: {}",
+                TwitterIndexModel.class.getName(), ids);
+
+        return ids;
+    }
+}
